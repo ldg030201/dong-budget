17ea1a0196e977c43ea6bb52f62845ebcfcf6955
ldg030201 <100822592+ldg030201@users.noreply.github.com>
2026-09-25 14:37:06 +0900

    🔖 0.1.6

--- app/src/main/java/com/dong/budget/ui/patchnotes/PatchNotes.kt
+++ app/src/main/java/com/dong/budget/ui/patchnotes/PatchNotes.kt
@@ -71,7 +71,7 @@ val PATCH_NOTES: List<Release> =
     listOf(
         Release(
             version = "0.1.6",
-            date = null,
+            date = LocalDate.of(2026, 9, 25),
             menus =
             listOf(
                 menu(
